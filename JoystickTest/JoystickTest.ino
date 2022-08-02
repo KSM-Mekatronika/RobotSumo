1733c1a389ccf8f0dac7efa0d64c8b6b926bf6b4
victoriavals <naufal.firdaus628@gmail.com>
2022-08-02 14:09:44 +0700

    changing program name

--- JoystickTest/JoystickTest.ino
+++ JoystickTest/JoystickTest.ino
@@ -1,13 +1,17 @@
-#define xpin 36
-uint16_t x_value;
+int x_Pin = 36; //Nama alias pin A1 yaitu xPin
+int y_Pin = 39; //Nama alias pin A2 yaitu xPin
+int hasilX;
+int hasilY;
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  pinMode(xpin, INPUT);
+Serial.begin(9600);
+pinMode(x_Pin, INPUT);
+pinMode(y_Pin, INPUT);
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  x_value = analogRead(xpin);
-  Serial.print(x_value);
+int x_Position = analogRead(x_Pin);
+int y_Position = analogRead(y_Pin);
+hasilX=map(x_Position,0,4095,0,255);
+hasilY=map(y_Position,0,4095,0,255);
+Serial.print("Nilai X : " + String(hasilX) + "\t" + "Nilai Y : " + String(hasilY) + "\n");
+delay(100);
 }
